9b884b5d832a5415a15dd4633ee7d2cd630c9bfa
shu00011 <koha.koishi.gnu.16@gmail.com>
2021-08-22 23:58:29 +0900

    [UPDATE] adjust

--- main.py
+++ main.py
@@ -8,34 +8,38 @@ import json
 with open('twitter.json') as f:
   twitter_keys = json.load(f)
 
-CONSUMER_KEY = twitter_keys['consumer_key']
-CONSUMER_SECRET = twitter_keys['consumer_secret']
-ACCESS_TOKEN_KEY = twitter_keys['access_token']
-ACCESS_TOKEN_SECLET = twitter_keys['access_token_secret']
+def main():
+  CONSUMER_KEY = twitter_keys['consumer_key']
+  CONSUMER_SECRET = twitter_keys['consumer_secret']
+  ACCESS_TOKEN_KEY = twitter_keys['access_token']
+  ACCESS_TOKEN_SECLET = twitter_keys['access_token_secret']
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECLET)
+  auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+  auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECLET)
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+  api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-public_tweets = api.home_timeline()
+  public_tweets = api.home_timeline()
 
-images =[]
+  img_urls = []
 
-for tweet in public_tweets:
-  if 'media' in tweet.entities:
-      for media in tweet.extended_entities['media']:
-          media_url = media['media_url']
-          images.append(media_url)
+  for tweet in public_tweets:
+    if 'media' in tweet.entities:
+        for media in tweet.extended_entities['media']:
+            media_url = media['media_url']
+            img_urls.append(media_url)
 
-for i in range(0,len(images)):
-  url = images[i]
-  file_name = f"./images/{i}.jpg"
-  response = requests.get(url)
-  image = response.content
-  with open(file_name, "wb") as f:
-    f.write(image)
+  for i in range(0,len(img_urls)):
+    url = img_urls[i]
+    file_name = f"./images/{i}.jpg"
+    response = requests.get(url)
+    img = response.content
+    with open(file_name, "wb") as f:
+      f.write(img)
 
-for i in range(0,len(images)):
-  img = Image.open(f'./images/{i}.jpg')
-  st.image(img, use_column_width=True)
+  for i in range(0,len(img_urls)):
+    img = Image.open(f'./images/{i}.jpg')
+    st.image(img, use_column_width=True)
+
+if __name__ == "__main__":
+  main()
