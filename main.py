2c1c4dd7afde63776e60657e4c2e44109cc9262e
shu00011 <koha.koishi.gnu.16@gmail.com>
2021-08-22 23:12:29 +0900

    finish

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import streamlit as st
+from PIL import Image
+import streamlit.components.v1 as components
 from requests_oauthlib import OAuth1Session
 import tweepy
+import requests
 import json
 with open('twitter.json') as f:
   twitter_keys = json.load(f)
@@ -16,8 +20,24 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 public_tweets = api.home_timeline()
 
+images =[]
+st.columns(5)
+
+
 for tweet in public_tweets:
-    if 'media' in tweet.entities:
-        for media in tweet.extended_entities['media']:
-            media_url = media['media_url']
-            print(media_url)
+  if 'media' in tweet.entities:
+      for media in tweet.extended_entities['media']:
+          media_url = media['media_url']
+          images.append(media_url)
+
+for i in range(0,len(images)):
+  url = images[i]
+  file_name = f"./images/{i}.jpg"
+  response = requests.get(url)
+  image = response.content
+  with open(file_name, "wb") as f:
+    f.write(image)
+
+for i in range(0,len(images)):
+  img = Image.open(f'./images/{i}.jpg')
+  st.image(img, use_column_width=True)
